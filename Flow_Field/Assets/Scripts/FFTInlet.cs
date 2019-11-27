625ff9cd3a533072817d94889da503134924bb97
Garrett <gflynn@usc.edu>
2019-11-26 19:31:32 -0800

    Added basic flowfield

---
+++ Flow_Field/Assets/Scripts/FFTInlet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using Assets.LSL4Unity.Scripts.AbstractInlets;
+
+/// <summary>
+/// Just an example implementation for a Inlet recieving float values
+/// </summary>
+public class FFTInlet : AFloatInlet
+{
+    public float[] FFT;
+
+    protected override void Process(float[] newSample, double timeStamp)
+    {
+        // just as an example, make a string out of all channel values of this sample
+        FFT = newSample;
+
+        Debug.Log(
+            string.Format("Got {0} samples at {1}", FFT.Length, timeStamp)
+            );
+    }
+}
---
+++ Flow_Field/Assets/Scripts/FocusInlet.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System;
+using System.Linq;
+using Assets.LSL4Unity.Scripts.AbstractInlets;
+
+    /// <summary>
+    /// Just an example implementation for a Inlet recieving float values
+    /// </summary>
+    public class FocusInlet : AIntInlet
+    {
+        public int[] Focus;
+
+        protected override void Process(int[] newSample, double timeStamp)
+        {
+            // just as an example, make a string out of all channel values of this sample
+            Focus = newSample;
+
+            Debug.Log(
+                string.Format("Got 1 samples at {0}", timeStamp)
+                );
+        }
+    }
